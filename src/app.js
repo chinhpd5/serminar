5d284c61f9a280a331a392af30d8f9f9613760c3
chinhpd5 <150000891+chinhpd5@users.noreply.github.com>
2025-02-17 23:22:16 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import 'dotenv/config'
 
 // Khởi tạo server
-const app = express();
+const app = express();aaaa
 // Khởi tạo cổng 
 const port = process.env.PORT || 3000;
 
